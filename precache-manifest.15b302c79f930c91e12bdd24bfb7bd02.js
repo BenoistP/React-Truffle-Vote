c0357c32543c43e9aec209c55d90680cb03bc66a
BenoistP <75934369+BenoistP@users.noreply.github.com>
2021-10-13 21:23:04 +0200

    Updates

---
+++ precache-manifest.15b302c79f930c91e12bdd24bfb7bd02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df3460f15c68bf8c4837d2257054e1a",
+    "url": "/React-Truffle-Vote/index.html"
+  },
+  {
+    "revision": "1045b3201060a77993a0",
+    "url": "/React-Truffle-Vote/static/css/2.a5ff5de7.chunk.css"
+  },
+  {
+    "revision": "9bc28b1f6a9ecd82adaa",
+    "url": "/React-Truffle-Vote/static/css/main.8729efe4.chunk.css"
+  },
+  {
+    "revision": "1045b3201060a77993a0",
+    "url": "/React-Truffle-Vote/static/js/2.1b02aeb4.chunk.js"
+  },
+  {
+    "revision": "9bc28b1f6a9ecd82adaa",
+    "url": "/React-Truffle-Vote/static/js/main.24ec4b90.chunk.js"
+  },
+  {
+    "revision": "fe2359dd93588bf76e6f",
+    "url": "/React-Truffle-Vote/static/js/runtime-main.3f9dff09.js"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/React-Truffle-Vote/static/media/getFetch.51c934af.cjs"
+  }
+]);
